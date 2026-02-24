3c3de53f8bf6a4e968c29894e390e7e6f5c5f4dd
ST10536628 <ST10536628@myemeris.edu.za>
2026-02-24 12:50:09 +0200

    first Submission

---
+++ src/project2/Project2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project2;
+
+/**
+ *
+ * @author ST10536628
+ */
+public class Project2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("\t\t\t----------------------\n");
+        System.out.println("\t ***********\t\t\t      xxxxxxxxxxxxxx");
+        System.out.println("\t *\t  *\t\t        X\t              x");
+    }
+    
+}
